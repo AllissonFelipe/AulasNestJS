f33eeded46d52f78aefa9e4251326f7f78a378d1
unknown <allissonfmbs95@gmail.com>
2025-11-10 12:16:13 -0300

    Bulletproof Validation: Setting Up Automatic Data Validation with Pipes

--- src/tasks/create-task.dto.ts
+++ src/tasks/create-task.dto.ts
@@ -1,7 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { TaskStatus } from './task.model';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsEnum(TaskStatus)
   status: TaskStatus;
 }
