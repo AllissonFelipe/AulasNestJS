9ff073b5f0ff074372b547e222f7d285e085e454
unknown <allissonfmbs95@gmail.com>
2025-11-10 13:00:39 -0300

    Validating and Transforming Data from the URL

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/c
 import { TasksService } from './tasks.service';
 import type { ITask } from './task.model';
 import { CreateTaskDto } from './create-task.dto';
+import { FindOneParams } from './find-one-params';
 
 @Controller('tasks')
 export class TasksController {
@@ -15,8 +16,8 @@ export class TasksController {
     }
 
     @Get('/:id')
-    public findOne(@Param('id') id: string): ITask {
-        const task = this.tasksService.findOne(id);
+    public findOne(@Param() params: FindOneParams): ITask {
+        const task = this.tasksService.findOne(params.id);
         if (task) {
         return task;
         }
