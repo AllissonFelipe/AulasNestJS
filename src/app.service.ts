7d854b9167dd86fd8990d5c8b8d4137614440ce6
unknown <allissonfmbs95@gmail.com>
2025-11-10 10:27:43 -0300

    Implementing the Logic: Fetching Data in a Service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Injectable, Logger } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { ITask } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
   @Get()
-  public findAll(): string[] {
-    return [`A`, `B`];
+  public findAll(): ITask[] {
+    return this.tasksService.findAll();
   }
 
   @Get('/:id')
-  public findOne(@Param('id') id: string): string {
+  public findOne(@Param('id') id: string): ITask {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    return `The number is ${id}`;
+    return this.tasksService.findOne(id);
   }
 }
