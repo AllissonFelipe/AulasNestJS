5f8e37c3cbf61faab74b33a47412c786a56fa107
unknown <allissonfmbs95@gmail.com>
2025-11-11 10:54:20 -0300

    Validating Configuration with Joi

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,5 +1,9 @@
 import { AppConfig } from './app.config';
-
+import * as Joi from 'joi';
 export interface ConfigType {
   app: AppConfig;
 }
+
+export const appConfigSchema = Joi.object({
+  APP_MESSAGE_PREFIX: Joi.string().default('Hello '),
+});
