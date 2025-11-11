eff8e37d5a0b954df982ad98b0f2f8665ce965e8
unknown <allissonfmbs95@gmail.com>
2025-11-11 10:36:50 -0300

    Type-Safe Configuration in NestJS

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,17 +3,19 @@ import { Injectable, Logger } from '@nestjs/common';
 import { DummyService } from './dummy/dummy.service';
 import { LoggerService } from './logger/logger.service';
 import { ConfigService } from '@nestjs/config';
+import { ConfigType } from './config/config.types';
+import { AppConfig } from './config/app.config';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly dummyService: DummyService,
     private readonly loggerService: LoggerService,
-    private readonly configService: ConfigService,
+    private readonly configService: ConfigService<ConfigType>,
   ) {}
 
   getHello(): string {
-    const prefix = this.configService.get<string>('app.messagePrefix');
+    const prefix = this.configService.get<AppConfig>('app')?.messagePrefix;
     return this.loggerService.log(
       `${prefix} Hello World! ${this.dummyService.work()}`,
     );
