61b212dce8dd78bfb9ee20851b9508cf8275b7fe
Allisson Felipe <allissonfmbs95@gmail.com>
2025-11-13 19:13:24 -0300

    Connecting TypeORM: Static vs Dynamic Configuration

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,7 +1,9 @@
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppConfig } from './app.config';
 import * as Joi from 'joi';
 export interface ConfigType {
   app: AppConfig;
+  database: TypeOrmModuleOptions;
 }
 
 export const appConfigSchema = Joi.object({
