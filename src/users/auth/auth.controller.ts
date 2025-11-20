20ea7f030514deefabfb8fe003cca3dac1391a5a
Allisson Felipe <allissonfmbs95@gmail.com>
2025-11-20 20:15:56 -0300

    Building as Admin-Only Area with a Role-Based Guard

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -17,6 +17,9 @@ import { LoginResponse } from '../login.response';
 import type { AuthRequest } from '../auth.request';
 import { UserService } from '../user/user.service';
 import { Public } from '../decorators/public.decorator';
+import { AdminResponse } from '../admin.response';
+import { Role } from '../role.enum';
+import { Roles } from '../decorators/roles.decorator';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -51,5 +54,11 @@ export class AuthController {
       return user;
     }
     throw new NotFoundException();
+  }
+
+  @Get('admin') // /auth/admin
+  @Roles(Role.ADMIN)
+  async adminOnly(): Promise<AdminResponse> {
+    return new AdminResponse({ message: 'This is for admins only!' });
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -13,6 +13,7 @@ import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthGuard } from './auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './roles.guard';
 
 @Module({
   imports: [
@@ -34,9 +35,14 @@ import { APP_GUARD } from '@nestjs/core';
     UserService,
     AuthService,
     AuthGuard,
+    RolesGuard,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
   ],
   controllers: [AuthController],
